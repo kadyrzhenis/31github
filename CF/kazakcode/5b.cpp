65ba7bcc73b64a520b60649a10dda1a7eea2941e
Zhekas K7 <47256333+kadyrzhenis@users.noreply.github.com>
2023-12-05 14:40:32 +0600

    as

---
+++ CF/kazakcode/5b.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define Do()                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);
+void solve()
+{
+    int n,m;
+    cin >> n>>m;
+    long long res=1;
+    int index = n-m;
+    for (int i = index+1; i <= n; i++)
+    {
+       res=res*i;
+    }
+    
+    cout << res << endl;
+}
+int main()
+{
+    Do() int t = 1;
+    while (t--)
+    {
+        solve();
+    }
+}
