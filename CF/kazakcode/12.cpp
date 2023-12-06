d0b71b9b2bf330d26cfece9058fb52138415ff6b
Zhekas K7 <47256333+kadyrzhenis@users.noreply.github.com>
2023-12-06 22:26:20 +0600

    refactoring

--- CF/kazakcode/12.cpp
+++ CF/kazakcode/12.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
-int main(){
-    unsigned long long a,b,c,res=0;
-    cin >> a>>b>>c;
-    unsigned long long m=10e12+7;
-    res=(a+b+c)%m;
-    cout <<res;
+int main()
+{
+    unsigned long long a, b, c, res = 0;
+    cin >> a >> b >> c;
+    unsigned long long m = 10e12 + 7;
+    res = (a + b + c) % m;
+    cout << res;
     return 0;
 }
--- CF/kazakcode/63.cpp
+++ CF/kazakcode/63.cpp
@@ -1,25 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-#define Do()   ios_base::sync_with_stdio(false); cin.tie(0);
+#define Do()                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);
 void solve()
 {
-    long long a,b,jup=0,taq=0;
+    long long a, b, jup = 0, taq = 0;
     cin >> a;
-    map<int,int>mp;
-    for(ll i=0;i<a;++i){
+    map<int, int> mp;
+    for (ll i = 0; i < a; ++i)
+    {
         ll k;
-        cin>>k;
+        cin >> k;
         mp[k]++;
     }
-    int mx=-1;
-    for(auto m:mp){
-        mx=max(mx,m.second);
+    int mx = -1;
+    for (auto m : mp)
+    {
+        mx = max(mx, m.second);
     }
-    for(auto m:mp){
-        if(mx==m.second){
-           cout<<m.first<<endl;
-           break;
+    for (auto m : mp)
+    {
+        if (mx == m.second)
+        {
+            cout << m.first << endl;
+            break;
         }
     }
 }
