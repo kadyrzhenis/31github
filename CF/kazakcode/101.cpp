0cf5cd92aef253de33b9b2d97bb1901e488a20e3
Zhekas K7 <47256333+kadyrzhenis@users.noreply.github.com>
2023-12-06 22:26:50 +0600

    update 101.cpp

--- CF/kazakcode/101.cpp
+++ CF/kazakcode/101.cpp
@@ -3,26 +3,34 @@ using namespace std;
 typedef long long ll;
 typedef vector<int> vi;
 #define all(v) v.begin(), v.end()
-#define Do()        ios_base::sync_with_stdio(false); cin.tie(0);
+#define Do()                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);
 void solve()
 {
-    vector<int>v;
+    vector<int> v;
     int k;
-    int mn=100000000;
-    while(cin>>k){
-       if(k!=0){
-           v.push_back(k);
-          mn = min(mn,k);
-       }else{
-           break;
-       }
+    int mn = 100000000;
+    while (cin >> k)
+    {
+        if (k != 0)
+        {
+            v.push_back(k);
+            mn = min(mn, k);
+        }
+        else
+        {
+            break;
+        }
     }
-    if(mn==100000000){
-        cout<<-1<<endl;
-    }else{
-      cout<<mn<<endl;
+    if (mn == 100000000)
+    {
+        cout << -1 << endl;
     }
-    
+    else
+    {
+        cout << mn << endl;
+    }
 }
 int main()
 {
