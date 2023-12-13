0c3406d922e4643b5a6bbc2db2f77148404419de
Zhekas K7 <47256333+kadyrzhenis@users.noreply.github.com>
2023-12-13 21:26:20 +0600

    deleted

--- codetime/e_2.cpp
+++ codetime/e_2.cpp
@@ -1,19 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-ll a[213344],b[213344];
+ll a[213344], b[213344];
 int main()
 {
-    ll n,m,x,y;
-    cin>>n>>m;
-    for(int i=0;i<n;i++)    cin>>a[i];
-    b[0]=a[0];
-    for(int i=1;i<n;i++)    b[i]=b[i-1]+a[i];
-    while(m--)
+    ll n, m, x, y;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    b[0] = a[0];
+    for (int i = 1; i < n; i++)
+        b[i] = b[i - 1] + a[i];
+    while (m--)
     {
-        cin>>x>>y;
-        x--,y--;
-        if((b[y]-b[x-1])%2!=0)    puts("Nao");
-        else    puts("Sim");
+        cin >> x >> y;
+        x--, y--;
+        if ((b[y] - b[x - 1]) % 2 != 0)
+            puts("Nao");
+        else
+            puts("Sim");
     }
 }
