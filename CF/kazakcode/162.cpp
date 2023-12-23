e8c58b820f88a1f7acc79a0f03aa8f9f82723dc6
Zhekas K7 <47256333+kadyrzhenis@users.noreply.github.com>
2023-12-23 23:24:30 +0600

    162 solve

---
+++ CF/kazakcode/162.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+#define all(v) v.begin(), v.end()
+#define Do()                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);
+void solve()
+{
+    vector<int> v;
+    int n;
+    cin>>n;
+    int arr[n][n],res[n][n];
+    for(int i=0;i<n;++i){
+        for(int j=0;j<n;++j){
+            cin>>arr[i][j];
+        }
+    }
+    for(int i=0;i<n;++i){
+        for(int j=0;j<n;++j){
+            res[j][i]=arr[i][j];
+        }
+    }
+    for(int i=0;i<n;++i){
+        for(int j=0;j<n;++j){
+            cout<<res[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+int main()
+{
+    Do() int t = 1;
+    while (t--)
+    {
+        solve();
+    }
+}
